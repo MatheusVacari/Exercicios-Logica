908ec03489b166e87cb184c79df46de527e4e711
MatheusVacari <matheusvacari@hotmail.com>
2019-03-11 11:47:11 -0300

    Experiêcia de trabalho, Júnior, Pleno, Sênior

---
+++ Exe6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exe6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Você está programando um sistema para uma empresa de recursos humanos.
+            // De acordo com o tempo de experiência do candidato você deve informar 
+            // ao recrutador o nível do candidato.Considere:
+
+            //   Menor que 2 anos → Júnior
+            //   Entre 2 e 5 anos → Pleno
+            //   Acima de 5 anos → Sênior
+
+            double Experiencia = 0;
+            string NomeCantidado;
+
+            Console.WriteLine("Informe o nome");
+                NomeCantidado = Console.ReadLine();
+
+            Console.WriteLine("Informe o tempo de experiência do candidato");
+                Experiencia = Convert.ToDouble(Console.ReadLine());
+
+            if(Experiencia <= 2)
+            {
+                Console.WriteLine("O candidato(a) " + NomeCantidado + " tem o nível: Júnior");
+            }
+            else if(Experiencia <= 5)
+            {
+                Console.WriteLine("O candidato(a) " + NomeCantidado + " tem o nível: Pleno");
+            }
+            else
+            {
+                Console.WriteLine("O candidato(a) " + NomeCantidado + " tem o nível: Sênior");
+            }
+        }
+    }
+}
