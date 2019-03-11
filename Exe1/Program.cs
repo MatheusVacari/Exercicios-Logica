08662ce86796682dabcbd07a28c23f89c9ba07f3
MatheusVacari <matheusvacari@hotmail.com>
2019-03-11 11:41:12 -0300

    Soma de dois valores e multiplicar pelo primeiro

---
+++ Exe1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 0, resultado = 0;
+
+            Console.WriteLine("Informe o primeiro valor!");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = (n1 * n2) * n1;
+
+            Console.Write("O resultado será: " + resultado);
+            
+        }
+    }
+}
